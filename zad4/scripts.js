6b3b8c449cc4eeb029d88ff33ec2dde894c6429e
Anastasiia Bzova <bzovanastia@gmail.com>
2026-04-13 00:32:26 +0200

    66617 - created logic of changing theme & color of toggle button

--- zad4/scripts.js
+++ zad4/scripts.js
@@ -1,3 +1,15 @@
 // Toggle between themes
 const themeStyle = document.getElementById('theme-style');
 const themeBtn = document.getElementById('theme-toggle');
+
+themeBtn.addEventListener('click', () => {
+    if (themeStyle.getAttribute('href') === './red.css') {
+        themeStyle.setAttribute('href', './green.css');
+        themeStyle.classList.remove('red-theme');   
+        themeStyle.classList.add('green-theme');
+    } else {
+        themeStyle.setAttribute('href', './red.css');
+        themeStyle.classList.remove('green-theme');
+        themeStyle.classList.add('red-theme');
+    }
+});
