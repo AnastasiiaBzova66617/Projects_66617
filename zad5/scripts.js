24eb7a12ef9e55b526b749d59bdd826a59b80d70
Anastasiia Bzova <bzovanastia@gmail.com>
2026-04-20 03:08:42 +0200

    66617 zad5: copy/paste files from zad4

---
+++ zad5/scripts.js
@@ -0,0 +1,27 @@
+// Toggle between themes
+const themeStyle = document.getElementById('theme-style');
+const themeBtn = document.getElementById('theme-toggle');
+
+themeBtn.addEventListener('click', () => {
+    if (themeStyle.getAttribute('href') === './red.css') {
+        themeStyle.setAttribute('href', './green.css');
+        themeBtn.textContent = 'Green Theme';
+    } else {
+        themeStyle.setAttribute('href', './red.css');
+        themeBtn.textContent = 'Red Theme';
+    }
+});
+
+// Toggle visibility of the section
+const projectsSection = document.getElementById('projects');
+const sectionBtn = document.getElementById('section-toggle');
+
+sectionBtn.addEventListener('click', () => {
+    if (projectsSection.style.display === 'none') {
+        projectsSection.style.display = 'block';
+        sectionBtn.textContent = 'Hide "Projects"';
+    } else {
+        projectsSection.style.display = 'none';
+        sectionBtn.textContent = 'Show "Projects"';
+    }
+});
